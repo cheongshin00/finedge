70e871af1aab6b5be0ad87fe804700594c4bca8a
Liew Cheong Shin <liewcheongshin@ecquaria.com>
2025-10-17 18:09:40 +0800

    Init project

---
+++ finedge-api/src/main/java/com/finedge/finedgeapi/FinedgeApplication.java
@@ -0,0 +1,11 @@
+package com.finedge.finedgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinedgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinedgeApplication.class, args);
+    }
+}
---
+++ finedge-common/src/main/java/org/finedge/Main.java
@@ -0,0 +1,7 @@
+package org.finedge;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
